c840efcea23fbde528a86683fa68e4e078b4c444
Torben Werner <torben.werner@yupiq.com>
2013-10-02 14:52:49 -0700

    TW: Some minor syntax fixes for script resource providers.

--- play/recipes/default.rb
+++ play/recipes/default.rb
@@ -6,14 +6,14 @@
 #
 #
 
-include_recipe "java"
-include_recipe "s3"
+include_recipe 'java'
 
 #Download play from s3
 if !File.directory?("#{node[:play][:lib_path]}")
-  # Get the packaged Play! distribution 
+  # Get the packaged Play! distribution
   if node[:play][:dist][:s3][:enabled]
     # Download Play! from S3 bucket (allows distro customization, direct AWS access, etc...)
+    include_recipe 's3'
     s3_aware_remote_file "#{Chef::Config[:file_cache_path]}/#{node[:play][:dist][:name]}.zip" do
       source "#{node[:play][:dist][:s3][:region]}://#{node[:play][:dist][:s3][:bucketname]}/#{node[:play][:dist][:name]}.zip"
       access_key_id node[:config][:aws][:s3][:access_key]
@@ -21,6 +21,7 @@ if !File.directory?("#{node[:play][:lib_path]}")
       owner "root"
       group "root"
       mode "644"
+      action :create
     end
   else
     # Download Play! from Typesafe's site or other file locations...
@@ -33,17 +34,12 @@ if !File.directory?("#{node[:play][:lib_path]}")
   end
 
   # Unzip the Play! distribution package
-  execute "unzip-play" do
+  bash "install-play-lib" do
     cwd Chef::Config[:file_cache_path]
-    command "unzip #{node[:play][:dist][:name]}.zip"
-  end
-
-  # Install Play!
-  execute "install-play-lib" do
-    command "mv #{Chef::Config[:file_cache_path]}/#{node[:play][:dist][:name]} #{node[:play][:lib_path]}"
-  end
-  
-  execute "install-play-bin" do
-    command "ln -s #{node[:play][:lib_path]}/play #{node[:play][:bin_path]}"
+    code <<-EOH
+    unzip #{node[:play][:dist][:name]}.zip
+    mv #{Chef::Config[:file_cache_path]}/#{node[:play][:dist][:name]} #{node[:play][:lib_path]}
+    ln -s #{node[:play][:lib_path]}/play #{node[:play][:bin_path]}
+    EOH
   end
 end
